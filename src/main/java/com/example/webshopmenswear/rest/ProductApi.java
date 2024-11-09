2367ada758028230c2d980d47b6ce701d4fa9cd5
Nguyen Dat <nguyentiendat1233333@gmail.com>
2024-11-10 00:24:29 +0700

    Search_product

--- src/main/java/com/example/webshopmenswear/rest/ProductApi.java
+++ src/main/java/com/example/webshopmenswear/rest/ProductApi.java
@@ -5,12 +5,14 @@ import com.example.webshopmenswear.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductApi {
     @Autowired
@@ -27,7 +29,6 @@ public class ProductApi {
         // Lấy dữ liệu sản phẩm từ service
         Page<Product> productSearch = productService.findByNameContainingAndStatus(name, true, 1, 10);
         // Trả về dữ liệu dưới dạng JSON
-        System.out.println("hahaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" + productSearch.getContent());
         return productSearch;
     }
 }
