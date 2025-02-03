964d39d9bdeeffbf4d6fb3ff3a7e8252578ab0c6
trunggdungg <le25011985@gmail.com>
2025-02-03 09:09:56 +0700

    API category

--- src/main/java/com/example/webshopmenswear/repository/CategoryRepository.java
+++ src/main/java/com/example/webshopmenswear/repository/CategoryRepository.java
@@ -3,5 +3,11 @@ package com.example.webshopmenswear.repository;
 import com.example.webshopmenswear.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Optional<Category> findByName(String name);
+
+    List<Category> findByNameContainingIgnoreCase(String keyword);
 }
--- src/main/java/com/example/webshopmenswear/service/CategoryService.java
+++ src/main/java/com/example/webshopmenswear/service/CategoryService.java
@@ -5,6 +5,7 @@ import com.example.webshopmenswear.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -18,6 +19,34 @@ public class CategoryService {
 
     public Category findById(Integer categoryId) {
         return categoryRepository.findById(categoryId).orElse(null);
+    }
+
+    public Category createCategory(Category category) {
+        category.setCreatedAt(LocalDateTime.now());
+        category.setUpdatedAt(LocalDateTime.now());
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> searchCategories(String keyword) {
+        return categoryRepository.findByNameContainingIgnoreCase(keyword);
+    }
+
+    public Category updateCategory(Integer categoryId, Category updatedCategory) {
+        return categoryRepository.findById(categoryId).map(category -> {
+            category.setName(updatedCategory.getName());
+            category.setSlug(updatedCategory.getSlug());
+            category.setUpdatedAt(LocalDateTime.now());
+            return categoryRepository.save(category);
+        }).orElse(null);
+    }
+
+    // Xóa loại sản phẩm
+    public boolean deleteCategory(Integer categoryId) {
+        if (categoryRepository.existsById(categoryId)) {
+            categoryRepository.deleteById(categoryId);
+            return true;
+        }
+        return false;
     }
 }
 
